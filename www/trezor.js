e1a89bfb26d220dc7191e4ec0d65fbb3cfcff657
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2020-07-23 16:35:31 +0430

    Convert paths to number arrays in Android

--- www/trezor.js
+++ www/trezor.js
@@ -1,7 +1,43 @@
 function CordovaTrezor() { };
 
-CordovaTrezor.prototype.getPublicKeys = function (request, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "CordovaTrezor", "getPublicKeys", [request]);
+CordovaTrezor.prototype._getHDPath = (path) => {
+    const parts = path.toLowerCase().split('/');
+    if (parts[0] !== 'm') throw 'Not a valid path';
+    return parts.filter((p) => p !== 'm' && p !== '')
+        .map((p) => {
+            let hardened = false;
+            if (p.substr(p.length - 1) === "'") {
+                hardened = true;
+                p = p.substr(0, p.length - 1);
+            }
+            let n = parseInt(p);
+            if (isNaN(n)) {
+                throw 'Not a valid path';
+            } else if (n < 0) {
+                throw 'Path cannot contain negative values';
+            }
+            if (hardened) { // hardened index
+                n = (n | 0x80000000) >>> 0;
+            }
+            return n;
+        });
+};
+
+CordovaTrezor.prototype._convertPathToArray = (path) => {
+    let valid = getHDPath(path);
+    if (!valid) throw 'Not a valid path';
+    return valid;
+};
+
+CordovaTrezor.prototype.getPublicKeys = function (request) {
+    let paths = request;
+    if (cordova.platformId === 'android') {
+        paths = { paths: request.bundle.map(p => this._convertPathToArray(p.path)) };
+    }
+
+    return new Promise((resolve, reject) => {
+        cordova.exec(resolve, reject, "CordovaTrezor", "getPublicKeys", [paths]);
+    });
 }
 
 module.exports = new CordovaTrezor();
